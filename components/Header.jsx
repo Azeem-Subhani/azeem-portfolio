98ba51606410da93d681a138211b5d6a7a8d5bae
Azeem Subhani <asubhani@cinnova.com>
2024-08-20 19:34:54 +0500

    Header Completed.
    Implemeted desktop header & Mobile view header.

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 
 // components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -24,7 +25,10 @@ const Header = () => {
         </div>
 
         {/* mobile nav*/}
-        <div className="xl:hidden">Mobile Navigation</div>
+        <div className="xl:hidden">
+
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
---
+++ components/MobileNav.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  { name: "home", path: "/" },
+  { name: "services", path: "/services" },
+  { name: "resume", path: "/resume" },
+  { name: "work", path: "/work" },
+  { name: "contact", path: "/contact" },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* Logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" className="text-xl" onClick={handleLinkClick}>
+            <h1 className="text-4xl font-semibold">
+              Azeem <span className="text-accent text-2xl">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* Nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              className={`${
+                link.path === pathname && "text-accent border-b-2 border-accent"
+              } text-xl capitalize hover:text-accent transition-all`}
+              onClick={handleLinkClick}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
