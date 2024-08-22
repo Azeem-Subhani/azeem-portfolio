69af6f2d581c9f1cdc9b0a22f9b70d0c57f4a21b
Azeem Subhani <asubhani@cinnova.com>
2024-08-22 16:58:18 +0500

    Added Resume Data Variables.

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,295 @@
+import { FaNodeJs, FaReact, FaJs, FaCss3, FaHtml5 } from "react-icons/fa";
+import {
+  SiExpress,
+  SiNextdotjs,
+  SiNestjs,
+  SiTailwindcss,
+  SiAwsamplify,
+  SiMysql,
+  SiMongodb,
+  SiMongoose,
+  SiSvelte,
+  SiAwslambda,
+  SiAmazonec2,
+  SiElasticsearch,
+  SiAmazondynamodb,
+  SiAmazoncognito,
+  SiFirebase,
+  SiAngular,
+  SiTypescript,
+  SiReactivex,
+  SiNgrx,
+  SiTypeorm,
+  SiRedux,
+  SiReduxsaga,
+  SiRecoil,
+  SiMobx,
+  SiReactquery,
+  SiServerless,
+  SiAmazoncloudwatch,
+  SiAwssecretsmanager,
+  SiAmazons3,
+  SiGraphql,
+  SiSupabase,
+  SiPostgresql,
+} from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ipsa quas architecto in, doloremque quam, quos voluptate voluptatem, molestias ea.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Azeem Subhani",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+92 320 4406148",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "azeemsubhani@proton.me",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "5+ Years",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Urdu",
+    },
+  ],
+};
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ipsa quas architecto in, doloremque quam, quos voluptate voluptatem, molestias ea.",
+  items: [
+    {
+      company: "Tech 1",
+      position: "Full stack developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Tech 1",
+      position: "Full stack developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Tech 1",
+      position: "Full stack developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Tech 1",
+      position: "Full stack developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "E-commerce Startup",
+      position: "Freelance Web Developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Digital Agency",
+      position: "Frontend Developer",
+      duration: "2022 - Present",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My Education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ipsa quas architecto in, doloremque quam, quos voluptate voluptatem, molestias ea.",
+  items: [
+    {
+      institution: "Comsats University Islamabad",
+      degree: "BSCS",
+      duration: "2022 - Present",
+    },
+    {
+      institution: "Test Dome",
+      degree: "Javascript & Node Developer",
+      duration: "2022 - Present",
+    },
+    {
+      institution: "Online Course",
+      degree: "The Ultimate Web Developer Bootcamp 2022",
+      duration: "2022",
+    },
+  ],
+};
+
+const skills = {
+  icon: "",
+  title: "My Skills",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ipsa quas architecto in, doloremque quam, quos voluptate voluptatem, molestias ea.",
+  items: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "TailwindCSS",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React",
+    },
+    {
+      icon: <SiAngular />,
+      name: "Angular",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Angular",
+    },
+    {
+      icon: <SiSvelte />,
+      name: "Svelte",
+    },
+    {
+      icon: <SiReactivex />,
+      name: "RxJs",
+    },
+    {
+      icon: <SiNgrx />,
+      name: "Ngxs",
+    },
+    {
+      icon: <SiRedux />,
+      name: "Redux",
+    },
+    {
+      icon: <SiReduxsaga />,
+      name: "Redux Sega",
+    },
+    {
+      icon: <SiRecoil />,
+      name: "Recoil",
+    },
+    {
+      icon: <SiMobx />,
+      name: "Mobx",
+    },
+    {
+      icon: <SiReactquery />,
+      name: "Tanstack Route/Query",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "NodeJs",
+    },
+    {
+      icon: <SiNestjs />,
+      name: "Nest JS",
+    },
+    {
+      icon: <SiServerless />,
+      name: "Serverless",
+    },
+    {
+      icon: <SiGraphql />,
+      name: "GraphQL",
+    },
+    {
+      icon: <SiExpress />,
+      name: "Tanstack Route/Query",
+    },
+    {
+      icon: <SiAwsamplify />,
+      name: "Amplify",
+    },
+    {
+      icon: <SiAwslambda />,
+      name: "Lambda",
+    },
+    {
+      icon: <SiAmazonec2 />,
+      name: "EC2",
+    },
+    {
+      icon: <SiElasticsearch />,
+      name: "Elastic Search",
+    },
+    {
+      icon: <SiAmazoncognito />,
+      name: "Cognito",
+    },
+    {
+      icon: <SiAmazoncloudwatch />,
+      name: "Cloud Watch",
+    },
+    {
+      icon: <SiAwssecretsmanager />,
+      name: "Secret Managet",
+    },
+    {
+      icon: <SiAmazons3 />,
+      name: "S3 Bucket",
+    },
+    {
+      icon: <SiAmazons3 />,
+      name: "S3 Bucket",
+    },
+    {
+      icon: <SiFirebase />,
+      name: "Firebase",
+    },
+    {
+      icon: <SiSupabase />,
+      name: "Supabase",
+    },
+    {
+      icon: <SiMongoose />,
+      name: "Mongoose",
+    },
+    {
+      icon: <SiTypeorm />,
+      name: "TypeORM",
+    },
+    {
+      icon: <SiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <SiPostgresql />,
+      name: "PostgreSQL",
+    },
+    {
+      icon: <SiAmazondynamodb />,
+      name: "DynamoDB",
+    },
+    {
+      icon: <SiMysql />,
+      name: "MySQL",
+    },
+  ],
+};
+
 const Resume = () => {
   return <div>Resume Page</div>;
 };
