a036e222c0b94b28216cafe34025f070b05f4c44
Azeem Subhani <asubhani@cinnova.com>
2024-08-29 17:19:43 +0500

    Worked on resume page.

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { FaNodeJs, FaReact, FaJs, FaCss3, FaHtml5 } from "react-icons/fa";
 import {
   SiExpress,
@@ -291,28 +292,87 @@ const skills = {
 };
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@radix-ui/react-tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@radix-ui/react-tooltip";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
 const Resume = () => {
   return (
-    <motion.div initial={{ opacity: 0 }}
-    animate={{
-      opacity: 1,
-      transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
-    }}
-    className="min-h-[80vh] flex items-center justify-center py-12 xl:py-8"
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh]  flex items-center justify-center py-12 xl:py-8"
     >
       <div className="container mx-auto">
-        <Tabs>
-          <TabsList>
-            <TabsTrigger>Experience</TabsTrigger>
-            <TabsTrigger>Education</TabsTrigger>
-            <TabsTrigger>Skills</TabsTrigger>
-            <TabsTrigger>About Me</TabsTrigger>
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
           </TabsList>
+
+          {/* Content */}
+          <div className="min-h-[70vh] w-full">
+            {/* Experience */}
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* Education */}
+            <TabsContent value="education" className="w-full">
+              Education
+            </TabsContent>
+
+            {/* Skills */}
+            <TabsContent value="skills" className="w-full">
+              Skills
+            </TabsContent>
+
+            {/* About */}
+            <TabsContent value="about" className="w-full">
+              About
+            </TabsContent>
+          </div>
         </Tabs>
       </div>
     </motion.div>
